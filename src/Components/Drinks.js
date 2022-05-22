b4fc4bebd7547cda6bc16f62101faf4ab96df462
Nathanael Hermanto <nathanael887@gmail.com>
2022-05-22 12:22:22 +0200

    feat: searchBar

--- src/Components/Drinks.js
+++ src/Components/Drinks.js
@@ -1,64 +1,103 @@
 import React from 'react'
-// import { useState } from 'react';
-// import { Cocktail, Ingredient } from './Cocktail';
+import { useState, useEffect } from 'react';
+import { Box } from '@mui/system';
+import { Cocktail, Ingredient } from './Cocktail';
+import CocktailCard from './Cards/CocktailCard';
 import SearchBar from './SearchBar';
 
-// const urlRandom = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-// const urlAList = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a'
-// const urlSearchDrink = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const urlSearchDrink = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
 const Drinks = () => {
-  // const [value, setValue] = useState('')
-  // const [data, setData] = useState([])
-  // const [isLoaded, setIsLoaded] = useState(false);
-  // const [error, setError] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-  // const search = (value) => {
-  //   const url = `${urlSearchDrink}value`
+  const search = () => {
+    const url = `${urlSearchDrink}${searchQuery}`
 
-  //   fetch(url)
-  //           .then(function(response){
-  //               return response.json();
-  //           })
-  //           .then(function(data) {
-  //               console.log(data.drinks[0]);
-  //               setIsLoaded(true);
-  //               setData(Cocktail(data.drinks[0].strDrink, 
-  //                               data.drinks[0].strCategory,
-  //                               [Ingredient(data.drinks[0].strIngredient1, data.drinks[0].strMeasure1), 
-  //                               Ingredient(data.drinks[0].strIngredient2, data.drinks[0].strMeasure2),
-  //                               Ingredient(data.drinks[0].strIngredient3, data.drinks[0].strMeasure3),
-  //                               Ingredient(data.drinks[0].strIngredient4, data.drinks[0].strMeasure4),
-  //                               Ingredient(data.drinks[0].strIngredient5, data.drinks[0].strMeasure5),
-  //                               Ingredient(data.drinks[0].strIngredient6, data.drinks[0].strMeasure6),
-  //                               Ingredient(data.drinks[0].strIngredient7, data.drinks[0].strMeasure7),
-  //                               Ingredient(data.drinks[0].strIngredient8, data.drinks[0].strMeasure8),],
-  //                               data.drinks[0].strGlass,
-  //                               data.drinks[0].strInstructions,
-  //                               data.drinks[0].strVideo,
-  //                               data.drinks[0].strDrinkThumb,
-  //                               data.drinks[0].strAlcoholic
-  //                       )
-  //                   )
-  //               },
-  //           function(error) {
-  //               setIsLoaded(true);
-  //               setError(error);
-  //               console.log("error: ", error.message)
-  //           })
+    fetch(url)
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data.drinks[0]);
+                setData(Cocktail(data.drinks[0].strDrink, 
+                                data.drinks[0].strCategory,
+                                [Ingredient(data.drinks[0].strIngredient1, data.drinks[0].strMeasure1), 
+                                Ingredient(data.drinks[0].strIngredient2, data.drinks[0].strMeasure2),
+                                Ingredient(data.drinks[0].strIngredient3, data.drinks[0].strMeasure3),
+                                Ingredient(data.drinks[0].strIngredient4, data.drinks[0].strMeasure4),
+                                Ingredient(data.drinks[0].strIngredient5, data.drinks[0].strMeasure5),
+                                Ingredient(data.drinks[0].strIngredient6, data.drinks[0].strMeasure6),
+                                Ingredient(data.drinks[0].strIngredient7, data.drinks[0].strMeasure7),
+                                Ingredient(data.drinks[0].strIngredient8, data.drinks[0].strMeasure8),],
+                                data.drinks[0].strGlass,
+                                data.drinks[0].strInstructions,
+                                data.drinks[0].strVideo,
+                                data.drinks[0].strDrinkThumb,
+                                data.drinks[0].strAlcoholic
+                        )
+                    )
+                },
+            function(error) {
+                setError(error);
+                console.log("error: ", error.message)
+            })
+  };
+
+  useEffect(() => {
+    fetch(apiUrl)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.drinks[0]);
+            setData(Cocktail(data.drinks[0].strDrink, 
+                            data.drinks[0].strCategory,
+                            [Ingredient(data.drinks[0].strIngredient1, data.drinks[0].strMeasure1), 
+                            Ingredient(data.drinks[0].strIngredient2, data.drinks[0].strMeasure2),
+                            Ingredient(data.drinks[0].strIngredient3, data.drinks[0].strMeasure3),
+                            Ingredient(data.drinks[0].strIngredient4, data.drinks[0].strMeasure4),
+                            Ingredient(data.drinks[0].strIngredient5, data.drinks[0].strMeasure5),
+                            Ingredient(data.drinks[0].strIngredient6, data.drinks[0].strMeasure6),
+                            Ingredient(data.drinks[0].strIngredient7, data.drinks[0].strMeasure7),
+                            Ingredient(data.drinks[0].strIngredient8, data.drinks[0].strMeasure8),],
+                            data.drinks[0].strGlass,
+                            data.drinks[0].strInstructions,
+                            data.drinks[0].strVideo,
+                            data.drinks[0].strDrinkThumb,
+                            data.drinks[0].strAlcoholic
+                    )
+                )
+            },
+        function(error) {
+            setError(error);
+            console.log("error: ", error.message)
+        })
+    }, [])
+
+  if (error) {
+    return <>Error: {error.message}</>;
+  } else {    
+    return (
+      <>
+        <Box>
+          <SearchBar 
+            onSubmit={search}
+            setSearchQuery={setSearchQuery}
+          />
+        </Box>
         
-
-  // }
-
-  return (
-    <>
-      <SearchBar
-                
-      />
-      <div>Drinks</div>
-    </>
-   
-  )
+        
+        <Box> 
+          <CocktailCard Cocktail={data} />
+        </Box>
+      </>
+    )
 }
 
-export default Drinks
+  
+};
+
+export default Drinks;
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { FormControl, TextField, InputAdornment } from '@mui/material';
+import { IconButton } from '@mui/material';
+import  SearchIcon  from '@mui/icons-material/Search' 
 
-const SearchBar = () => {
+const SearchBar = ({ setSearchQuery, onSubmit }) => {
   return (
-    <div>SearchBar</div>
+    <>
+      <FormControl
+        onSubmit={onSubmit}
+        fullWidth>
+        <TextField
+          fullWidth
+          onInput={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+          variant="outlined"
+          placeholder="Search a drink"
+          InputProps={{
+            endAdornment:  (
+              <InputAdornment position="end">
+                <IconButton type="submit" aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ) 
+          }}
+          sx={{mt: 1, mb: 1, }}
+        />
+      </FormControl>
+    </>
   )
 }
 
